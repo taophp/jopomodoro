ca7c9ed07379c46b57cbac5212bfa4fe6c7b2eeb
Stéphane Mourey <mail@stephanemourey.fr>
2024-01-30 22:46:40 +0100

    First UI

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,20 @@ import joplin from 'api';
 
 joplin.plugins.register({
 	onStart: async function() {
-		// eslint-disable-next-line no-console
-		console.info('Hello world. Test plugin started!');
+		const panel = await joplin.views.panels.create('pom_1');
+		await joplin.views.panels.setHtml(panel, `
+			<h1>🍅</h1>
+			<div id="timer">
+				<span id="running">00:00</span><br>
+				<span id="length">00:00</span>
+			</div>
+			<div id="controls">
+				<button title="(Re)Start pomodoro">▶️</button>
+				<button title="Suspend">⏸️</button>
+				<button title="Reset timer">🔁</button>
+				<button title="Short pause">⏹️</button>
+				<button title="Long pause">⏏️</button>
+			</div>
+		`);
 	},
 });
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+npm run dist
+/tmp/.mount_joplinAmFdae/@joplinapp-desktop --env dev
